d3ff96f198a354c9552d518d9f272c0f628eefbe
bene-s-dev <development@stehmans.de>
2026-05-29 21:27:42 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+javascript
+const CACHE_NAME = 'rehawalk-cache-v1';
+
+// Diese Dateien werden für die Offline-Nutzung gespeichert
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install-Event: Cachen der App-Dateien
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch-Event: Daten aus dem Cache laden, falls offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Falls gefunden, lade aus dem Cache
+        if (response) {
+          return response;
+        }
+        // Ansonsten lade normal aus dem Netzwerk
+        return fetch(event.request);
+      })
+  );
+});
